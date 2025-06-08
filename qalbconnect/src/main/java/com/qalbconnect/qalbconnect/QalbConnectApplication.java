2deb58758be3d2bb73b77ad184d9606c407cccb9
Abdul Rafay <abdulrafaymehmood123@gmail.com>
2025-06-08 07:59:24 -0700

    initialized the spring boot app

---
+++ qalbconnect/src/main/java/com/qalbconnect/qalbconnect/QalbConnectApplication.java
@@ -0,0 +1,13 @@
+package com.qalbconnect.qalbconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalbConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalbConnectApplication.class, args);
+	}
+
+}
---
+++ qalbconnect/src/test/java/com/qalbconnect/qalbconnect/QalbConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qalbconnect.qalbconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalbConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
